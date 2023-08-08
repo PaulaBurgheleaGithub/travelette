d331d2942a1adc61e51fed569ad9646a103efaeb
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-08-08 22:57:57 +0100

    DB migration completed. Started working on the CRUD APIs

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [transactions, setTransactions] = useState("");
+
+  return (
+    <>
+      <div>
+    <h2>Travelette App</h2>
+
+      </div>
+    </>
+  )
+}
+
+export default App
